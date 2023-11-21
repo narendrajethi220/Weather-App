c2528fbf9e6b0eb5cc9268aecca8ebb9ee87d310
Narendra Jethi <narendrajethi220@gmail.com>
2023-11-21 10:53:15 +0530

    Project Completed.
    1)  Successfully implemented API calls in the Weather App.
    2)  Now displays city name, weather condition, temperature, and icon.
    3) Added a refresh button for user convenience.

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,21 @@
-const lat = 30.31;
-const lon = 78.03;
-
 link =
-  "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=690a670f8c34d75916f4b6048004e007";
+  "https://api.openweathermap.org/data/2.5/weather?lat=30.31&lon=78.03&appid={Your API Keys}";
+var request = new XMLHttpRequest();
+request.open("GET", link, true);
+request.onload = function () {
+  var obj = JSON.parse(this.response);
+  console.log(obj);
+  document.getElementById("weather").innerHTML = obj.weather[0].description;
+  document.getElementById("location").innerHTML = obj.name;
+  document.getElementById("temp").innerHTML = Math.round(
+    obj.main.temp - 273.15
+  );
+  document.getElementById("icon").src =
+    "https://openweathermap.org/img/w/" + obj.weather[0].icon + ".png";
+};
+if (request.status >= 200 && request.status < 400) {
+  var temp = obj.main.temp;
+} else {
+  console.log("The City data is not available");
+}
+request.send();
